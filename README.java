0327d86793d387018b90e54404537da9d6f9b4b3
Silent-xiaomiao <Silent_xiaomiao@outlook.com>
2025-07-11 09:56:17 +0800

    NEW README.java

--- README.java
+++ README.java
@@ -5,7 +5,21 @@ import java.util.function.Function;
 public class WriteReadme {
     public static void main(String[] args) {
         String content = """
+        - 👋 我是@Silent-xiaomiao
+        - 🌑 是SilentStudio的成员
+        > 🏅 一群有志青年聚集在这里🙃
+        > 🏵️ [当然，如果有点中年人士也没关系。但不包括我😁]
+        - 💻 我对Java，Python，C, c#, c++，VB,HTML…
+        - 🌱 我目前正在学习Python应用程序开发
+        - 💰 我正在寻找合作…
+        - 📡 怎么联系我？电子邮件:Silent_xiaomiao@outlook.com
+        - ✏️ 简称(外号)：小喵
+        - 🈲 趣事：…
+-------------------——🇨🇳——-----------------——🇨🇳——------------------——🇨🇳——------------------      
         - 👋 Hi, I’m @Silent-xiaomiao
+        - 🪐 I belong to SilentStudio.
+        > 🗣️ A large number of young people with aspirations gather here.🙃        
+        > 👥 [Of course, it doesn't matter if a bit middle-aged.😁]
         - 👀 I’m interested in Java,Python,C,C#,C++,VB,HTML…
         - 🌱 I’m currently learning Python Application development
         - 💞️ I’m looking to collaborate on ...
